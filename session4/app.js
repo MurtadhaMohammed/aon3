92f3dbf2f0891642d3e5b9d9d9bfd022d9a73139
MurtadhaMohammed <mumu9943@gmail.com>
2025-10-03 19:50:10 +0300

    update

---
+++ session4/app.js
@@ -0,0 +1,36 @@
+// let username = "ali";
+// let password = "888888";
+// var age = 50;
+
+// let obj = {
+//   id: 1,
+//   name: "salim",
+//   age: 34,
+// };
+
+// let arr = [1000, 12, "Ali", 10, 0, obj];
+// let isDone = false;
+
+// console.log(arr);
+
+// console.log("this is: " + username);
+// console.log(`this is: ${password}`);
+// console.log(`age is : ${age}`);
+
+// console.log(isDone);
+
+// let number = 100;
+
+// if (number >= 100) {
+//   console.log("More Than 100");
+// } else {
+//   console.log("Less Than 100");
+// }
+
+let arr = [1000, 12, 10, 0, 90];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < 12) {
+    console.log(arr[i]);
+  }
+}
